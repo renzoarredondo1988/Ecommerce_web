aae60e7a915c903fcab8d42ee6b5530fef842683
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2024-10-02 01:03:37 -0300

    modified: agrego validaciones basicas en registro.js

--- ecommerce/GestionUsuarios/templates/GestionUsuarios/registro.js
+++ ecommerce/GestionUsuarios/templates/GestionUsuarios/registro.js
@@ -7,11 +7,40 @@ formRegister.addEventListener('submit', (event) => {
 
 const registerUser = () => {
     let nombre = document.getElementById('register_name').value;
+    const validNombre = /^[A-Za-z\s]+$/;
+    if (!validNombre.test(nombre)) { //validación del nombre
+        viewAlert("Error", "El nombre solo debe contener letras.");
+        return; // Detiene el envío del formulario
+    }
+
     let apellido = document.getElementById('register_lastname').value;
+    const validApellido = /^[A-Za-z\s]+$/;
+    if (!validApellido.test(apellido)) { //validación del apellido
+        viewAlert("Error", "El apellido solo debe contener letras.");
+        return; // Detiene el envío del formulario
+    }
+
     let pais = document.getElementById('register_country').value;
+
     let correo = document.getElementById('register_email').value;
+    const validCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!validCorreo.test(correo)) {
+        viewAlert("Error", "Por favor, ingresa un correo válido.");
+        return;
+    }
+
     let contraseña = document.getElementById('register_password').value;
+    const validContraseña = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; // Al menos 8 caracteres, una letra y un número
+    if (!validContraseña.test(contraseña)) {
+        viewAlert("Error", "La contraseña debe tener al menos 8 caracteres, incluyendo una letra y un número.");
+        return;
+    }
+
     let confirmar_contraseña = document.getElementById('confirm_password').value;
+    if (contraseña !== confirmar_contraseña) {
+        viewAlert("Error", "Las contraseñas no coinciden. Intenta de nuevo");
+        return;
+    }
 
     console.log(nombre, apellido, pais, correo, contraseña, confirmar_contraseña);
 
@@ -45,5 +74,3 @@ const registerUser = () => {
         viewAlert("Error!", "Algo ha salido mal. Intenta de nuevo");
     })
 }
-
-
