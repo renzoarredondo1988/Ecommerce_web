8ca08c620ce0809f79440fae92ebe5fe2e09b19e
Yessica Reynoso <andrea.reynoso@kdsoft.io>
2024-10-09 21:43:55 -0300

    Agrego últimos cambios en el back

--- ecommerce/Productos/admin.py
+++ ecommerce/Productos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Producto, Categoria, Juego  # Importa los modelos que deseas registrar
+
+# Registra los modelos para que aparezcan en el panel de administración
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Juego)
--- ecommerce/Productos/urls.py
+++ ecommerce/Productos/urls.py
@@ -1,8 +1,23 @@
-# En urls.py de tu aplicación Productos
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/', views.listar_productos, name='listar_productos'),  # URL para listar productos
-    path('productos/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),  # URL para el detalle del producto
+    # Rutas para la API
+    path('api/catalogo/', views.api_mostrar_catalogo, name='api_mostrar_catalogo'),
+    path('api/producto/<int:producto_id>/', views.api_detalle_producto, name='api_detalle_producto'),
+    path('api/buscar/', views.api_buscar_productos, name='api_buscar_productos'),
+    path('api/categorias/', views.obtener_categorias, name='api_obtener_categorias'),
+    path('api/juegos/', views.api_ver_juegos, name='api_ver_juegos'),
 ]
+
+
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/productos/', views.api_ver_productos, name='api_ver_productos'),
+    path('api/categorias/', views.api_ver_categorias, name='api_ver_categorias'),
+    path('api/juegos/', views.api_ver_juegos, name='api_ver_juegos'),
+]
+"""
