0190f445ff2e0160dc036b8133d92a8aabdd8cdf
Renzo Arredondo <renzo.mau1988@gmail.com>
2024-10-11 22:33:31 -0300

    se crea la app pagos, se pasa todo lo hecho en carritos a pagos

--- ecommerce/Carrito/views.py
+++ ecommerce/Carrito/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 
-#Integracion carrito
-import mercadopago
-from django.conf import settings
-from django.shortcuts import render
+
 
 def carrito(request):
     if request.method == 'POST':
@@ -13,51 +10,15 @@ def carrito(request):
         #cuando se necesita cambiar de url porque nos dirigimos a otra pagina se usa redirect y no render.
         #al movernos a esa nueva URL, se llama a la funcion def metodo_pago_x.
         if metodo_pago == '1':
-            return redirect('metodo_pago_1')
+            return redirect('pagos:metodo_pago_1')
         elif metodo_pago == '2':
-            return redirect('metodo_pago_2')
+            return redirect('pagos:metodo_pago_2')
     return render(request,"Carrito/carrito.html")  # Aquí iría la lógica para mostrar el contenido del carrito
 
-def metodo_pago_1(request):
-    return render(request,"Carrito/metodoPago1.html")
 
-def metodo_pago_2(request):
-    return render(request,"Carrito/metodoPago2.html")
 
 
 def agregar_al_carrito(request, producto_id):
     pass  # Aquí iría la lógica para agregar un producto al carrito
 
 
-
-def metodo_pago_mercadopago(request):
-    # Inicializar el SDK de MercadoPago con el access_token
-    sdk = mercadopago.SDK(settings.MERCADO_PAGO_ACCESS_TOKEN)
-
-    # Crear la preferencia de pago
-    preference_data = {
-        "items": [
-            {
-                "title": "Producto Ejemplo",
-                "quantity": 1,
-                "unit_price": 100.00
-            }
-        ],
-        "back_urls": {
-            "success": "http://tu-dominio.com/success",
-            "failure": "http://tu-dominio.com/failure",
-            "pending": "http://tu-dominio.com/pending"
-        },
-        "auto_return": "approved",  # Redirecciona automáticamente al éxito si el pago es aprobado
-    }
-
-    preference_response = sdk.preference().create(preference_data)  # Creación de la preferencia
-    preference = preference_response["response"]
-
-    # Pasamos la public_key al template junto con la preferencia ID
-    context = {
-        "preference_id": preference['id'],
-        "public_key": settings.MERCADO_PAGO_PUBLIC_KEY
-    }
-
-    return render(request, 'Carrito/checkout.html', context)
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'Carrito',
     'InterfazGeneral',
     'PagosyPedidos',
+    'pagos',
 
 ]
 
@@ -139,5 +140,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+#INTEGRACION DE MERCADOPAGO
 MERCADO_PAGO_ACCESS_TOKEN = 'APP_USR-5285721843882809-101119-da1cfb4649ba02083771e8f3bf6a83ad-238786920'
 MERCADO_PAGO_PUBLIC_KEY = 'APP_USR-3a6866da-6e61-4cc3-9215-4b3e6d9fec70'
