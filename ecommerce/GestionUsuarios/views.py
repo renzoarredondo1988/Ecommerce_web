7919c0cb798b0dd9662f8e919bc880e20a35ba5f
Tiago Schaerer <tiago_06@hotmail.es>
2024-10-29 20:46:35 -0300

    login y registro funcionando ok sin json

--- ecommerce/GestionUsuarios/views.py
+++ ecommerce/GestionUsuarios/views.py
@@ -2,7 +2,7 @@ import json  # Importamos el módulo json para trabajar con datos en formato JSO
 from django.http import JsonResponse  # Importamos JsonResponse para enviar respuestas JSON
 from django.shortcuts import render
 from .models import Validacion  # Importamos el modelo Validacion
-from django.contrib.auth.hashers import make_password  # Importamos para hash de contraseñas
+from django.contrib.auth.hashers import make_password,check_password   # Importamos para hash de contraseñas
 from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def registerUser(request):
@@ -43,24 +43,27 @@ def registerUser(request):
 
 
 
-
+@csrf_exempt
 def login_user(request):
     if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            email = data.get('email')
-            password = data.get('password')
-        except json.JSONDecodeError:
-            return JsonResponse({'success': False, 'message': 'Datos JSON inválidos.'}, status=400)
+        email = request.POST.get('login_email')
+        password = request.POST.get('login_password')
 
-        # Verificar si el correo existe en la base de datos
         try:
             usuario = Validacion.objects.get(correo=email)
             if check_password(password, usuario.contraseña):  # Verificar la contraseña
                 return JsonResponse({'success': True, 'message': 'Inicio de sesión exitoso'}, status=200)
             else:
-                return JsonResponse({'success': False, 'message': 'Contraseña incorrecta'}, status=401)
+                return render(request, 'GestionUsuarios/login.html', {
+                    'error_message': 'Contraseña incorrecta.'
+                })
         except Validacion.DoesNotExist:
-            return JsonResponse({'success': False, 'message': 'Correo no registrado'}, status=404)
-
-    return JsonResponse({'success': False, 'message': 'Método no permitido'}, status=405)
+            return render(request, 'GestionUsuarios/login.html', {
+                'error_message': 'Correo no registrado.'
+            })
+        
+        
+#guardamso aca los nuevos usuario?
+#comentar en la meet
+#cerrar
+    return render(request, 'GestionUsuarios/login.html')
